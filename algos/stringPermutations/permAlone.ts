41108aee47f8e2e6b33addb8e8ddc21a28b74196
Pavel Popov <pcveykos@gmail.com>
2022-10-24 15:41:34 +0200

    Add work on permAlone function

--- algos/stringPermutations/permAlone.ts
+++ algos/stringPermutations/permAlone.ts
@@ -1,6 +1,37 @@
-export default function permAlone(str:string):number {
-    
-    return 1
+export default function permAlone(str:string) {
+    const permHolder:string[] = []
+    if (str.length === 1) {
+        return str
+    }
+    for (let i = 0; i< str.length; i++) {
+        let char = str[i]
+        let remainingChars = str.slice(0,i) + str.slice(i+1)
+
+        for (let permutation of permAlone(remainingChars)){
+            permHolder.push(char+permutation)
+        }
+    }
+
   }
   
-  permAlone('aab');
+
+//   export default function permAlone(str:string, indx?:number):string[] | string {
+//     const permHolder:string[] = []
+//     if (str.length === 1) {
+//         return str
+//     }
+//     for (let i = 0; i< str.length; i++) {
+//         let char = str[i]
+//         let remainingChars = str.slice(0,i) + str.slice(i+1)
+
+//         for (let permutation of permAlone(remainingChars)){
+//             permHolder.push(char+permutation)
+//         }
+//     }
+//     const pattern = new RegExp('([a-z])\\1')
+//     return permHolder.filter(ele => !pattern.test(ele))
+//   }
+  
+
+
+// permAlone('aba')
