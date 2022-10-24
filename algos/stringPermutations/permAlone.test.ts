5b22049cbfdbd08e704e6c6766ef88b2e234c4c3
Pavel Popov <pcveykos@gmail.com>
2022-10-24 16:07:54 +0200

    Add final version of permAlone

--- algos/stringPermutations/permAlone.test.ts
+++ algos/stringPermutations/permAlone.test.ts
@@ -13,4 +13,13 @@ describe('permAlone should', () => {
     test('function should return a number', () => {
         expect(typeof permAlone('asd')).toBe('number')
     })
+    test('should return proper result with simple string', () => {
+        expect(permAlone('aab')).toBe(2)
+    })
+    test('should return 0 if no possible permutation without consecutive duplicates', () => {
+        expect(permAlone('abaa')).toBe(0)
+    })
+    test('should return 1 if string length is 1', () => {
+        expect(permAlone('a')).toBe(1)
+    })
 })
--- algos/stringPermutations/permAlone.ts
+++ algos/stringPermutations/permAlone.ts
@@ -1,19 +1,26 @@
-export default function permAlone(str:string) {
-    const permHolder:string[] = []
-    if (str.length === 1) {
-        return str
+export default function permAlone(str: string) {
+	function permHelper(stri: string): string[] {
+    if (stri.length === 1) {
+      return [stri];
     }
-    for (let i = 0; i< str.length; i++) {
-        let char = str[i]
-        let remainingChars = str.slice(0,i) + str.slice(i+1)
+    const permHolder: string[] = [];
+    for (let i = 0; i < stri.length; i++) {
+      // take the character at position i
+      let char = stri[i];
+      // slice the remaining characters off the string before and after char at pos i
+      // and concatenate them
+      let remainingChars = stri.slice(0, i) + stri.slice(i + 1);
 
-        for (let permutation of permAlone(remainingChars)){
-            permHolder.push(char+permutation)
-        }
+			// recurse back into the function
+      for (let permutation of permHelper(remainingChars)) {
+        permHolder.push(char + permutation);
+      }
     }
-
+    return permHolder;
   }
-  
+  const pattern = new RegExp("([a-z])\\1");
+  return permHelper(str).filter((ele) => !pattern.test(ele)).length;
+}
 
 //   export default function permAlone(str:string, indx?:number):string[] | string {
 //     const permHolder:string[] = []
@@ -31,7 +38,5 @@ export default function permAlone(str:string) {
 //     const pattern = new RegExp('([a-z])\\1')
 //     return permHolder.filter(ele => !pattern.test(ele))
 //   }
-  
-
 
 // permAlone('aba')
